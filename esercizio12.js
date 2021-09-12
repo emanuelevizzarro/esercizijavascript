0e2ab50d6e1c1d6afdd2306293fc3a24fa2c251f
emanuelevizzarro <86508509+emanuelevizzarro@users.noreply.github.com>
2021-09-12 22:07:00 +0100

    Delete esercizio12.js

--- esercizio12.js
+++
@@ -1,9 +0,0 @@
-/*const concat = function(arr1, arr2) {
-    return arr1.concat(arr2);
-}
-
-console.log(concat([2, 4], [5, 1])); */
-
-const concat = (arr1, arr2) => arr1.concat(arr2)
-
-console.log(concat([2, 4], [5, 1]));
